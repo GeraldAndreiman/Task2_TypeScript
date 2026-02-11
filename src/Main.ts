9a599ef4f2bcaa4902b27da318fd020f00ebdccb
Gerald Andreiman <geraldandreiman5b@gmail.com>
2026-02-11 14:32:24 +0200

    Added imported supplier and added categories in main file

---
+++ src/Main.ts
@@ -0,0 +1,13 @@
+import type { Supplier } from "./data/Supplier";
+
+let categories: string[] = ["Electronics","Clothing","Books","Accessories"];
+type Category = "Electronics" | "Clothibg" | "Books" | "Accessories";
+
+const supplier: Supplier {
+
+    id: 1;
+    Name: "Euronix"
+    Address: "Tallinn pst 3";
+    ContactEmail: "EuroNix@gmail.com"
+
+}
